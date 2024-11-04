25f5ba4c7ce51ca94bf28c27e92e472e72b383ac
Jorge <jorge1b3@hotmail.es>
2024-11-03 20:48:05 -0500

    Feat/mapper (#3)
    * fix: rename old folders to sale project
    
    * feat: use mapstruct mapper to replace custom mapper

--- src/main/java/com/masa/sale/mapper/CartMapper.java
+++ src/main/java/com/masa/sale/mapper/CartMapper.java
@@ -1,32 +1,13 @@
 package com.masa.sale.mapper;
 
 import com.masa.sale.dto.CartDTO;
-import com.masa.sale.dto.CartItemDTO;
 import com.masa.sale.model.Cart;
-import com.masa.sale.model.CartItem;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-import java.util.stream.Collectors;
-
-@Component
-public class CartMapper {
-    public CartDTO toDTO(Cart cart) {
-        if (cart == null) return null;
-
-        return CartDTO.builder()
-                .id(cart.getId())
-                .enabled(cart.getEnabled())
-                .items(cart.getCartItems().stream()
-                        .map(this::toCartItemDTO)
-                        .collect(Collectors.toSet()))
-                .build();
-    }
-
-    private CartItemDTO toCartItemDTO(CartItem cartItem) {
-        return CartItemDTO.builder()
-                .cartId(cartItem.getCartId())
-                .itemId(cartItem.getItemId())
-                .quantity(cartItem.getQuantity())
-                .build();
-    }
+@Mapper(componentModel = "spring", uses = CartItemMapper.class)
+public interface CartMapper {
+    @Mapping(target = "items", source = "cartItems")
+    CartDTO toDTO(Cart cart);
+    Cart toEntity(CartDTO cartDTO);
 }
