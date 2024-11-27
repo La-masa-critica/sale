8fb471bba24adfb53d85ea904ded71a2d79cccab
jorge1b3 <jorge1b3@hotmail.es>
2024-11-27 12:49:41 -0500

    🐞 fix: remove cors

--- src/main/java/com/masa/sale/config/WebConfig.java
+++ src/main/java/com/masa/sale/config/WebConfig.java
@@ -1,17 +1,17 @@
-package com.masa.sale.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowedOriginPatterns("*")
-                .allowPrivateNetwork(true);
-    }
-}
+//package com.masa.sale.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedMethods("*")
+//                .allowedHeaders("*")
+//                .allowedOriginPatterns("*")
+//                .allowPrivateNetwork(true);
+//    }
+//}
